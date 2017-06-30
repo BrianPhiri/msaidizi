7080beb2ff8c02142dc31bd88eee17c2f82ec31c
BrianPhiri <brianphiri.9523@gmail.com>
2017-06-30 22:57:22 +0300

    retrive requests

--- app/src/main/java/blvck/chausiku/nick/msaidizi/adapter/RequestAdapter.java
+++ app/src/main/java/blvck/chausiku/nick/msaidizi/adapter/RequestAdapter.java
@@ -1,10 +1,18 @@
 package blvck.chausiku.nick.msaidizi.adapter;
 
 import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 import blvck.chausiku.nick.msaidizi.R;
 
@@ -16,10 +24,17 @@ public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.ViewHold
     private static final int LENGTH = 18;
 
     Context context;
+    public List<String> requestsDataTitle;
+    public List<String> requestsDataMessage;
 
-    public RequestAdapter(Context context) {
-        this.context = context;
+    public RequestAdapter(FragmentActivity activity, List<String> requestsDataTitle, List<String> requestsDataMessage) {
+        this.context = activity;
+        this.requestsDataTitle = requestsDataTitle;
+        this.requestsDataMessage = requestsDataMessage;
+
     }
+
+
 
     @Override
     public RequestAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -27,24 +42,62 @@ public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.ViewHold
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        holder.title.setText(requestsDataTitle.get(position));
+        holder.message.setText(requestsDataMessage.get(position));
+        holder.read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                View inflater = LayoutInflater.from(context).inflate(R.layout.dialog_readmore, null);
 
+                final TextView message = (TextView) inflater.findViewById(R.id.more_information_txt);
+                message.setText(requestsDataMessage.get(position));
+
+                Button button = (Button) inflater.findViewById(R.id.accept_request);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //
+                    }
+                });
+
+                builder.setView(inflater);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+        holder.accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //
+            }
+        });
     }
-
 
     @Override
     public int getItemCount() {
-        return LENGTH;
+        return requestsDataTitle.size();
     }
 
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title, message;
+        Button accept, read;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+        }
         public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.item_card, parent, false));
+            title = (TextView) itemView.findViewById(R.id.card_title);
+            message = (TextView) itemView.findViewById(R.id.card_text);
+            accept = (Button) itemView.findViewById(R.id.action_button);
+            read = (Button) itemView.findViewById(R.id.read_btn);
         }
     }
 
-    public void showDialog(){
-        Toast toast = Toast.makeText(context,"",Toast.LENGTH_LONG);
-        toast.show();
-    }
 }
--- app/src/main/java/blvck/chausiku/nick/msaidizi/fragment/RequestListFragment.java
+++ app/src/main/java/blvck/chausiku/nick/msaidizi/fragment/RequestListFragment.java
@@ -4,20 +4,40 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import blvck.chausiku.nick.msaidizi.R;
 import blvck.chausiku.nick.msaidizi.adapter.RequestAdapter;
+import blvck.chausiku.nick.msaidizi.models.Request;
+
+import static com.google.android.gms.internal.zzs.TAG;
 
 /**
  * Created by brianphiri on 6/28/17.
  */
 
 public class RequestListFragment extends Fragment  {
+
+    private List<String> requestsDataTitle = new ArrayList<>();
+    private List<String> requestsDataMessage = new ArrayList<>();
+    RequestAdapter requestAdapter;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("requests");
 
     Button button;
 
@@ -27,19 +47,32 @@ public class RequestListFragment extends Fragment  {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
-                R.layout.recycler_view, container, false);
-        RequestAdapter adapter = new RequestAdapter(recyclerView.getContext());
-        recyclerView.setAdapter(adapter);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
-        View ss = inflater.inflate(R.layout.item_tile,container,false);
+        final RecyclerView recyclerView = (RecyclerView) inflater.inflate( R.layout.recycler_view, container, false);
 
-        button = (Button) ss.findViewById(R.id.tile_item_btn);
+        ValueEventListener valueEventListener = myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                requestsDataMessage.clear();
+                requestsDataTitle.clear();
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    Request request = snapshot.getValue(Request.class);
+                    requestsDataMessage.add(request.getDescription());
+                    requestsDataTitle.add(request.getServiceType());
+                }
+                requestAdapter = new RequestAdapter(getActivity(),requestsDataTitle,requestsDataMessage);
+                recyclerView.setAdapter(requestAdapter);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+
+        });
 
         return recyclerView;
     }
